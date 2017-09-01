bb85a386ff558f2cd278b70c171d7a9572394399
Eligijus Stugys <eligijus.stugys@nfq.lt>
2017-09-01 16:59:26 +0300

    Lastest changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { RouterModule } from '@angular/router';
-import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +25,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    AppRoutingModule // <-- Module responsible for routing
+    AppRoutingModule, // <-- Module responsible for routing
+    HttpModule // <-- The HttpModule from the @angular/http library holds providers for a complete set of HTTP services.
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
